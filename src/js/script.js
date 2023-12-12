f576168e8db7a29a394e58231e66aa4d100b4515
Victor <victor.fsilva06@gmail.com>
2023-12-11 23:16:01 -0300

    inclusão de section dos videos com biblioteca .js de carrosel

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slides-video", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 800,
+    pagination: {
+      el: ".s-videos .swiper-pagination",
+      clickable: true,
+    },
+  });
