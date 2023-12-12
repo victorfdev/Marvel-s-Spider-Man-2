174f133b41a90ab7a2498a4b03d66e843f7df51c
Victor <victor.fsilva06@gmail.com>
2023-12-12 09:26:24 -0300

    Adição de animações e hover

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,8 @@ var swiper = new Swiper(".slides-video", {
       el: ".s-videos .swiper-pagination",
       clickable: true,
     },
-  }); 
+    navigation: {
+      nextEl: ".s-videos .btn-next",
+      prevEl: ".s-videos .btn-prev",
+    },
+  });
